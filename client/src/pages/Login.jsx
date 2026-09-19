450c224191ad641992154498df54aff6083d67d6
Prachi Bodhe <prachibodhe08@gmail.com>
2026-09-19 14:16:23 +0530

    Fix login API URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://recipebox-backend-s0xb.onrender.com/api/auth/login",
         {
           email,
           password
